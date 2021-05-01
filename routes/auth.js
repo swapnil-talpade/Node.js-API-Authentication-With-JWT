626dd81171f9cd6d0369d9c3e24eca55e334c490
Swapnil Talpade <swapniltalpade41@gmail.com>
2021-05-01 16:52:50 +0530

    Hashing password using bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../model/User");
 const { registerValidation } = require('../validation');
+const bcrypt = require('bcryptjs');
 
 
 // REGISTER
@@ -10,17 +11,25 @@ router.post("/register", async (req, res) => {
     const { error } = registerValidation(req.body)
     if (error) return res.status(400).send(error.details[0].message)
 
+    // check if user is already in db
+    const emailExist = await User.findOne({ email: req.body.email })
+    if (emailExist) return res.status(400).send('Email already exists')
+
+    // hash the passwords
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
 
     // create new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashPassword
     });
     try {
 
         const savedUser = await user.save();
-        res.send(savedUser)
+        res.send({ user: user._id })
     } catch (error) {
         res.status(400).send(error)
     }
